ab122f4c99b9373f1b0a8bc2560910d29cbcfb11
Arkady Wey <arkady.wey2@faculty.ai>
2025-04-25 17:40:02 +0100

    Add preproccesing exercise

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/ex_4_train_test_split.py
@@ -0,0 +1,18 @@
+# Import necessary libraries
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Load the Iris dataset
+df = pd.read_csv("iris.csv")
+
+# Separate features and target
+X = df.iloc[:,0:-1].values 
+y = df.iloc[:, -1].values
+
+# Split the dataset into an 80-20 training-test set
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, train_size=0.8, random_state=1)
+
+print(X_train.shape)
+print(X_test.shape)
+print(X.shape)
--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/ex_data_preprocessing.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/ex_data_preprocessing.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Import dataset 
+# -----
 dataset = pd.read_csv(filepath_or_buffer="./Data.csv")
 print(dataset.head())
 
@@ -14,6 +15,7 @@ print(X)
 print(y)
 
 # Taking care of missing data
+# -----
 # Could delete the row
 # Could replace the missing value by the average of the column 
 from sklearn.impute import SimpleImputer
@@ -24,6 +26,8 @@ X[:, 1:3] = imputer.transform(X[:, 1:3]) # Actually does the replacement
 print(X)
 
 # Encoding categorical data - turning something like string data into number
+# -----
+
 # We can't just use numbers, because we don't want the model to think these entries have an order that matters
 # Instead, we use one hot encoding - creating identity vectors 
 from sklearn.compose import ColumnTransformer
@@ -38,3 +42,17 @@ label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 
 print(y)
+
+# Splitting dataset into training and testing 
+# -----
+# Note we need to do this BEFORE we scale the features
+# This is because the test set is cosidered completely independent of the train set - it's distribvution shouldn't be considered for scaling the train set
+# So we do scaling after, to prevent what is called INFORMATION LEAKAGE
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=1)
+
+print(X_train)
+print(X_test)
+
+# Feature scaling
+# -----
