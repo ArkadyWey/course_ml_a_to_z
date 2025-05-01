f5876dc91681bb56dda4c1c0374ccc3e9ef7fbe0
Arkady Wey <arkadywey@googlemail.com>
2025-05-01 17:41:39 +0100

    Add linearity rules for multilinear regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/ex_multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/ex_multiple_linear_regression.py
@@ -27,10 +27,19 @@ linear_regressor = linear_regressor.fit(X=X_train, y=y_train)
 
 # Predicting the Test set results
 y_pred = linear_regressor.predict(X=X_test)
-
 # Evaluate 
 score_train = linear_regressor.score(X=X_train,y=y_train)
 score_test = linear_regressor.score(X=X_test,y=y_test)
 print(score_train)
 print(score_test)
 
+
+"""
+Assumptions of Linear Regresssion 
+* Linearity 
+* Homoscedasticity - equal variance around the mean as you traverse the x axis - i.e., var is not a factor of the x var 
+* Multivariate normaility - normal distribution around the mean
+* Independence - there is no pattern in our data i.e. not the stock market, where past price ictates future price 
+* Predictors should not be correlated
+* No serious outliers - if so, then remove
+"""
